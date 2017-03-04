002771554d90126720736711880026e385cc9742
larrylikestocode <wangwei@dhcp-128-189-69-36.ubcsecure.wireless.ubc.ca>
2017-03-04 08:22:52 -0800

    update
    Working on the real trajectory

---
+++ Trajectory/Assets/C#Script/RenderBallisticPath.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderBallisticPath : MonoBehaviour {
+
+	public float initialVelocity = 10.0f;
+	public float timerResolution = 0.02f;
+	public float maxTime = 10.0f;
+
+	private LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () {
+		lineRenderer = GetComponent<LineRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+
+	{
+		Vector3 velocityVector = transform.forward * initialVelocity;
+		Vector3 currentPosition = transform.position;
+		int index = 0; 
+
+		lineRenderer.numPositions =(int)(maxTime/timerResolution);
+
+
+		for (float t = 0.0f; t < maxTime; t += timerResolution) 
+		{
+			lineRenderer.SetPosition(index,currentPosition);
+			currentPosition+=velocityVector*timerResolution;
+			velocityVector += Physics.gravity*timerResolution;
+			index++;
+		}
+
+
+
+	}
+}
